c9e1b1086abb8211506ca1c3470f7575b17d251f
Evan <awesomeyggd@gmail.com>
2023-12-21 10:54:16 +0800

    2023 day 19

--- 2023/day19/solution.py
+++ 2023/day19/solution.py
@@ -1,6 +1,7 @@
 import re
+import copy
 
-with open('./test', 'r') as f:
+with open('./input', 'r') as f:
   data = f.read().strip()
 
 
@@ -66,5 +67,55 @@ def solve(workflows, ratings):
   print(total)
 
 
+def recur(c, wn, workflows):
+  rules = workflows[wn]
+  total = 0
+  for rule in rules[:-1]:
+    nc = copy.deepcopy(c)
+    e, n = rule.split(':')
+    k, o, v = e[0], e[1], int(e[2:])
+    if o == '>':
+      nc[k][0] = v + 1 if v > nc[k][0] else nc[k][0]
+    else:
+      nc[k][1] = v - 1 if v < nc[k][1] else nc[k][1]
+    if n == 'A':
+      t = 1
+      for cc in nc:
+        t *= (nc[cc][1] - nc[cc][0] + 1)
+      total += t
+    elif n != 'R':
+      total += recur(nc, n, workflows)
+    if o == '<':
+      c[k][0] = v if v > c[k][0] else c[k][0]
+    else:
+      c[k][1] = v if v < c[k][1] else c[k][1]
+
+  last = rules[-1]
+  nc = copy.deepcopy(c)
+  if last == 'A':
+    t = 1
+    for cc in nc:
+      t *= (nc[cc][1] - nc[cc][0] + 1)
+    total += t
+  elif last != 'R':
+    total += recur(nc, last, workflows)
+  return total
+
+
+def solve_2(workflows):
+  workflows = parse_workflows(workflows)
+  c = {
+    'x': [1, 4000],
+    'm': [1, 4000],
+    'a': [1, 4000],
+    's': [1, 4000],
+  }
+
+  total = recur(c, 'in', workflows)
+  print(total)
+
+
 workflows, ratings = [p.split('\n') for p in data.split('\n\n')]
 solve(workflows, ratings)
+
+solve_2(workflows)
