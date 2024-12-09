c50ad4deb3133fa32e3e93f6c2433dbb9f370246
Evan <awesomeyggd@gmail.com>
2024-12-09 14:00:03 +0800

    2024 day 07 - part two

--- 2024/day07/solution.py
+++ 2024/day07/solution.py
@@ -1,43 +1,47 @@
-with open('./input', 'r') as f:
-  data = f.read().strip()
+with open("./input", "r") as f:
+    data = f.read().strip()
+
 
 def dfs(nums, d, cur, tv):
-  if d == len(nums):
-    return cur == tv
-  sum = cur + nums[d]
-  prod = nums[d] if d == 0 else nums[d] * cur
-  return dfs(nums, d + 1, sum, tv) or dfs(nums, d + 1, prod, tv)
+    if d == len(nums):
+        return cur == tv
+    sum = cur + nums[d]
+    prod = nums[d] if d == 0 else nums[d] * cur
+    return dfs(nums, d + 1, sum, tv) or dfs(nums, d + 1, prod, tv)
 
 
 def solve1():
-  sum = 0
-  for line in data.split('\n'):
-    tvs, arrs = line.split(':')
-    tv = int(tvs)
-    nums = [int(n) for n in arrs.strip().split(' ')]
+    sum = 0
+    for line in data.split("\n"):
+        tvs, arrs = line.split(":")
+        tv = int(tvs)
+        nums = [int(n) for n in arrs.strip().split(" ")]
 
-    if dfs(nums, 0, 0, tv):
-      sum += tv
-  return sum
-  
-print(f'part one: {solve1()}')
+        if dfs(nums, 0, 0, tv):
+            sum += tv
+    return sum
 
 
-def dfs2(nums, d, e, tv):
-  return
+print(f"part one: {solve1()}")
 
+
+def dfs2(nums, d, tv, acc):
+  if d == len(nums):
+      return acc == tv
+  sum = acc + nums[d]
+  prod = nums[d] if d == 0 else nums[d] * acc
+  concat = nums[d] if d == 0 else int(str(acc) + str(nums[d]))
+  return dfs2(nums, d+1, tv, sum) or dfs2(nums, d+1, tv, prod) or dfs2(nums, d+1, tv, concat)
 
 def solve2():
-  sum = 0
-  for line in data.split('\n'):
-    tvs, arrs = line.split(':')
-    tv = int(tvs)
-    nums = [n for n in arrs.strip().split(' ')]
-    print(f'test value: {tv}', end='\r')
-    if dfs2(nums, 1, nums[0], tv):
-      sum += tv
-  print('\r')
-  return sum
+    sum = 0
+    for line in data.split("\n"):
+        tvs, arrs = line.split(":")
+        tv = int(tvs)
+        nums = [int(n) for n in arrs.strip().split(" ")]
+        if dfs2(nums, 0, tv, 0):
+            sum += tv
+    return sum
 
 
-print(f'part two: {solve2()}')
+print(f"part two: {solve2()}")
